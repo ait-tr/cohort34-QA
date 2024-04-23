8929ab05d8f7e1f4d2542d28befe5742c124af5e
Ira Baker <49585685+irinabaker@users.noreply.github.com>
2024-04-23 13:41:39 +0200

    Lesson_15

--- qa/lesson_15/code/QA34_ilCarro_Cucumber/src/main/java/com/ilcarro/pages/LoginPage.java
+++ qa/lesson_15/code/QA34_ilCarro_Cucumber/src/main/java/com/ilcarro/pages/LoginPage.java
@@ -1,8 +1,12 @@
 package com.ilcarro.pages;
 
+import io.cucumber.datatable.DataTable;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+import java.util.Map;
 
 public class LoginPage extends BasePage{
 
@@ -35,6 +39,17 @@ public class LoginPage extends BasePage{
 
     public LoginPage isMessageDisplayed(String text) {
         assert message.getText().contains(text);
+        return this;
+    }
+
+    public LoginPage enterInvalidPassword(DataTable table) {
+
+        List<Map<String,String>> dataTable = table.asMaps();
+
+        String email = dataTable.get(0).get("email");
+        String password = dataTable.get(0).get("password");
+
+        enterData(email,password);
         return this;
     }
 }
---
+++ qa/lesson_15/homework.md
@@ -0,0 +1,5 @@
+## Homework
+
+1. Повторите, пожалуйста, действия, которые мы делали на уроке в Fiddler, с [вебсайтом](https://telranedu.web.app/home). Отчет предоставьте в виде документа в Google Drive с логом
+
+2. Установите, пожалуйста, Postman https://www.postman.com/downloads/
